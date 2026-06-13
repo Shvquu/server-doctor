9b28186d1e87ae2daf617e0c99c66beccd573a30
shvquu <contact@shvquu.de>
2026-06-13 12:08:52 +0200

    Set up Maven publishing for `serverdoctor-api` and add GitHub Actions workflow for API release deployment

--- serverdoctor-api/build.gradle.kts
+++ serverdoctor-api/build.gradle.kts
@@ -1,6 +1,35 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
 dependencies {
     api(project(":serverdoctor-common"))
 
     testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            groupId = "com.serverdoctor.api"
+            artifactId = "serverdoctor-api"
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Shvquu/server-doctor")
+
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
