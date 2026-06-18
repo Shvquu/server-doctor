8576cf41477ca0c8f34e27a19b1a72d7cf8bafe9
shvquu <contact@shvquu.de>
2026-06-18 17:02:36 +0200

    bump: update project version to 0.9.0 and clean up BungeeCord module dependencies

--- serverdoctor-bungeecord/build.gradle.kts
+++ serverdoctor-bungeecord/build.gradle.kts
@@ -5,8 +5,6 @@ dependencies {
     implementation(project(":serverdoctor-storage"))
     implementation(project(":serverdoctor-rest-api"))
     implementation(project(":serverdoctor-webhook"))
-
-    implementation(libs.snakeyaml)            // nur für das config.yml-Parsing
-
-    compileOnly(libs.bungeecord.api)
+    implementation(libs.snakeyaml)
+    compileOnly(libs.bungeecord.api)   // KEIN annotationProcessor
 }
